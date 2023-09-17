2aa6b9001658f62130bd29c05c7f47df3a346b95
Rok <rok.krivicic@dhimahi.com>
2023-09-17 17:56:19 +0200

    Added navigation items and test for them

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,14 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li v-for="menuIem in menuItems" :key="menuIem" class="ml-9 h-full first:m-0">
+              <a href="" class="flex h-full items-center py-2.5">{{ menuIem }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -13,7 +20,9 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Rok Careers'
+      company: 'Rok Careers',
+      url: 'https://www.google.com/',
+      menuItems: ['Teams', 'Locations', 'Life at Rok Careers', 'How we hire', 'Students', 'Jobs']
     };
   }
 };
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -8,4 +8,18 @@ describe('MainNav', () => {
     const company = screen.getByText('Rok Careers');
     expect(company).toBeInTheDocument();
   });
+
+  it('displays menu item for navigation', () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole('listitem');
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.textContent);
+    expect(navigationMenuTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Rok Careers',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ]);
+  });
 });

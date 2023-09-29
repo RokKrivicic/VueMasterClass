a710be7f6ea08fd6201a39dd6d5449eabe2d0580
Rok <rok.krivicic@dhimahi.com>
2023-09-29 17:28:44 +0200

    Added dynamic routes, query params for jobs result page

---
+++ tests/unit/components/JobSearch/JobSearchForm.test.js
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
+
+import JobSearchForm from '@/components/JobSearch/JobSearchForm.vue';
+import { vi } from 'vitest';
+
+describe('JobSearchForm', () => {
+  describe('when user submits form', () => {
+    it('directs user to jobs results page with user search paramaters', async () => {
+      const push = vi.fn();
+      const $router = { push };
+
+      render(JobSearchForm, {
+        global: {
+          mocks: {
+            $router
+          },
+          stubs: {
+            FontAwesomeIcon: true
+          }
+        }
+      });
+
+      const roleInput = screen.getByRole('textbox', {
+        name: /role/i
+      });
+
+      await userEvent.type(roleInput, 'Vue Developer');
+
+      const locationInput = screen.getByRole('textbox', {
+        name: /where?/i
+      });
+
+      await userEvent.type(locationInput, 'Ohio');
+
+      const submitButton = screen.getByRole('button', {
+        name: /search/i
+      });
+
+      await userEvent.click(submitButton);
+
+      expect(push).toHaveBeenCalledWith({
+        name: 'JobResults',
+        query: {
+          role: 'Vue Developer',
+          location: 'Ohio'
+        }
+      });
+    });
+  });
+});

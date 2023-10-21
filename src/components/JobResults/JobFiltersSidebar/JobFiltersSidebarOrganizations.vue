7c82e5eb42c010398d77ca9f3a45b23ee75b0575
Rok <rok.krivicic@dhimahi.com>
2023-10-21 12:44:24 +0200

    Added first slot, added component CollapsibleAccordion and wrote the tests for it

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,30 @@
+<template>
+  <collapsible-accordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li v-for="organization in organizations" :key="organization" class="h-8 w-1/2">
+            <input :id="organization" type="checkBox" class="mr-3" />
+            <label :for="organization">{{ organization }}</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </collapsible-accordion>
+</template>
+
+<script>
+import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue';
+
+export default {
+  name: 'JobFiltersSidebarOrganizations',
+  components: {
+    CollapsibleAccordion
+  },
+  data() {
+    return {
+      organizations: ['Vuetube', 'Between Vue', 'Et Vue Brute', 'Vue and a Half Men']
+    };
+  }
+};
+</script>

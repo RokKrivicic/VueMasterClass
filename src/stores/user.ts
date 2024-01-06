d11508b7fed5d4082cbc962b2df71f1a743c7503
Rok <rok.krivicic@dhimahi.com>
2024-01-06 19:03:21 +0100

    Finish with the last section

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -8,6 +8,7 @@ export const useUserStore = defineStore('user', () => {
   const selectedOrganizations = ref<string[]>([]);
   const selectedJobTypes = ref<string[]>([]);
   const selectedDegrees = ref<string[]>([]);
+  const skillsSearchTerm = ref('');
 
   const loginUser = () => {
     isLoggedIn.value = true;
@@ -29,6 +30,11 @@ export const useUserStore = defineStore('user', () => {
     selectedOrganizations.value = [];
     selectedJobTypes.value = [];
     selectedDegrees.value = [];
+    skillsSearchTerm.value = '';
+  };
+
+  const UPDATE_SKILLS_SEARCH_TERM = (term: string) => {
+    skillsSearchTerm.value = term;
   };
 
   return {
@@ -36,10 +42,12 @@ export const useUserStore = defineStore('user', () => {
     selectedOrganizations,
     selectedJobTypes,
     selectedDegrees,
+    skillsSearchTerm,
     loginUser,
     ADD_SELECTED_ORGANIZATIONS,
     ADD_SELECTED_JOB_TYPES,
     ADD_SELECTED_DEGREES,
-    CLEAR_JOB_FILTER_SELECTIONS
+    CLEAR_JOB_FILTER_SELECTIONS,
+    UPDATE_SKILLS_SEARCH_TERM
   };
 });
